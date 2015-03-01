2c45350d8c5b0c26e0527d760b65d4397725e082
Kabir Kang <kangkabir@gmail.com>
2015-02-28 16:08:39 -0800

    App checks if network is connected and gets JSON with hardcoded coordinates

---
+++ app/src/main/java/com/kabirkang/stormy/AlertDialogFragment.java
@@ -0,0 +1,27 @@
+package com.kabirkang.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.FragmentManager;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Kabir on 2/28/15.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+}
